5a8b8b8a571034168e8114b721705c2572e46bb8
tomilinBV <qwertere97@mail.ru>
2021-03-29 17:57:46 +0300

    Commit_10.15

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -11,25 +11,34 @@ def main():
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    prime_list = []
+
+    for num in range(1, 101):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                prime_list.append(num)
 
     for i in range(0, 3):
         value = random.randint(1, 101)
         print('Question: {}'.format(value))
         check = prompt.string('Your answer: ')
 
-        if (value == 2 or value == 3) and check == 'yes':
-            print('Correct!')
-        elif value == 2 and check == 'no':
+        if check not in ['yes', 'no']:
+            print('Only "yes" or "no"!')
+            break
+
+        if value not in prime_list and check == 'yes':
             print("'yes' is wrong answer ;(. Correct answer was 'no'.\n",
                   "Let's try again, {}!".format(name))
             break
-        elif (value % 2 == 0 or value % 3 == 0 or value % 5 == 0) and check == 'yes':
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.\n",
+        elif value in prime_list and check == 'no':
+            print("'no' is wrong answer ;(. Correct answer was 'yes'.\n",
                   "Let's try again, {}!".format(name))
             break
-        elif (value % 2 != 0 or value % 3 != 0) and check == 'yes':
-            print('Correct!')
-        elif (value % 2 == 0 or value % 3 == 0 or value % 5 == 0) and check == 'no':
+        else:
             print('Correct!')
 
         if i == 2:
